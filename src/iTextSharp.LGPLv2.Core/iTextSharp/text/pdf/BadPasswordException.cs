bd66dbe7da316657a2bb737185cafec80ff157e4
kurema <kurema_makoto_software@yahoo.co.jp>
2023-10-03 05:33:12 +0900

    Update BadPasswordException to support TryPassword()

--- src/iTextSharp.LGPLv2.Core/iTextSharp/text/pdf/BadPasswordException.cs
+++ src/iTextSharp.LGPLv2.Core/iTextSharp/text/pdf/BadPasswordException.cs
@@ -1,16 +1,35 @@
-namespace iTextSharp.text.pdf;
+﻿namespace iTextSharp.text.pdf;
 
 public class BadPasswordException : IOException
 {
-    public BadPasswordException(string message) : base(message)
-    {
-    }
+	public BadPasswordException(string message) : base(message)
+	{
+	}
 
-    public BadPasswordException()
-    {
-    }
+	public BadPasswordException()
+	{
+	}
 
-    public BadPasswordException(string message, Exception innerException) : base(message, innerException)
-    {
-    }
+	public BadPasswordException(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	public BadPasswordException(string message, Func<byte[], PasswordTestResult> tester) : this(message)
+	{
+		this.Tester = tester;
+	}
+
+	public PasswordTestResult? TryPassword(byte[] password)
+	{
+		if (Tester is null) return null;
+		return Tester.Invoke(password);
+	}
+	public bool CanTryPassword { get { return Tester != null; } }
+
+	internal Func<byte[], PasswordTestResult> Tester { get; }
+
+	public enum PasswordTestResult
+	{
+		SuccessOwnerPassword, SuccessUserPassword, Fail,
+	}
 }
